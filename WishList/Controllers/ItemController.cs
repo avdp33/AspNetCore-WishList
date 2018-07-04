3dd804643b12859b24cabe296d6861620e105de3
avdp33 <avdp33@hotmail.nl>
2018-07-04 11:54:21 +0200

    6.2

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Item> items = _context.Items.ToList();
+            return View("Index", items);
+        }
+
+        
+    }
+}
